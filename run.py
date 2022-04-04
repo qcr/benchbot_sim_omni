79e88488afa1be9d849fccea91e6fc30af915559
root <b.talbot@qut.edu.au>
2022-04-04 23:42:23 +0000

    Add untested logic for managing simulation state

--- run.py
+++ run.py
@@ -31,27 +31,40 @@ class SimulatorDaemon:
 
         self.inst = None
         self.sim = None
+        self.sim_i = 0
 
         self.map_usd = None
         self.robot_usd = None
         self.start_pose = None
 
     def open_usd(self):
+        # Stop simulation if running
+        self.stop_simulation()
+
         # TODO figure out API calls for this...
         pass
 
-        # TODO start simulation if appropriate
+        # Attempt to start the simulation
+        start_simulation()
 
     def place_robot(self):
-        p = DEFAULT_POSE if self.start_pose is None else self.start_pose
+        # Stop simulation if running
+        self.stop_simulation()
 
         # Add robot to the environment at the requested pose
+        p = DEFAULT_POSE if self.start_pose is None else self.start_pose
         add_reference_to_stage(usd_path=self.robot_path,
                                prim_path=ROBOT_PRIM_PATH)
         r = Robot(prim_path=ROBOT_PRIM_PATH, name=ROBOT_NAME)
         r.set_world_pose(position=p[4::] * 100, orientation=p[:4])
 
-        # TODO start simulation if appropriate
+        # Disable auto-publishing of all robot components (we'll manually
+        # publish at varying frequencies instead)
+        for p in ROBOT_COMPONENTS.values():
+            disable_component(p)
+
+        # Attempt to start the simulation
+        start_simulation()
 
     def run(self):
         f = flask.Flask(__name__)
@@ -86,24 +99,25 @@ class SimulatorDaemon:
 
         @f.route('/restart', methods=['POST'])
         def __restart():
-            stop()
-            start()
+            stop_simulation()
+            start_simulation()
 
         @f.route('/start', methods=['POST'])
         def __start():
-            start()
+            start_simulation()
 
         @f.route('/stop', methods=['POST'])
         def __stop():
-            stop()
+            stop_simulation()
 
         server = pywsgi.WSGIServer(self.address, f)
 
-    def start(self):
+    def start_instance(self):
         if not self.inst is None:
             print("Instance already running. Please /stop first.")
             return
         env = {} if self.map_usd is None else {"open_usd": self.map_usd}
+
         # Start the simulator
         self.inst = SimulationApp({
             "renderer": "RayTracedLighting",
@@ -125,14 +139,34 @@ class SimulatorDaemon:
         if self.robot_usd is not None:
             self.place_robot()
 
-        # TODO start simulation if appropriate
+        # Attempt to start the simulation
+        start_simulation()
 
-    def stop(self):
+    def start_simulation(self):
+        if self.sim is not None:
+            self.stop_simulation()
+        if self.inst is None or self.map_usd is None or self.robot_usd is None:
+            print("Can't start simulation. Missing some required state.")
+            return
+
+        self.sim_i = 0
+        self.sim = SimulationContext()
+        self.sim.play()
+
+    def stop_instance(self):
         if self.inst is None:
             print("No instance is running to stop.")
             return
         self.inst.close()
         self.inst = None
+
+    def stop_simulation(self):
+        if self.sim is None:
+            print("Skipping. No running simulation to stop")
+        if self.inst is None:
+            print("Skipping. No running simulator found.")
+        self.sim.stop()
+        self.sim = None  # TODO maybe could reuse with more guarding logic?
 
 
 if __name__ == '__main__':
