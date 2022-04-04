1635a357baeefb9993959ca22b9f3b9b15824225
root <b.talbot@qut.edu.au>
2022-04-04 23:27:15 +0000

    Flesh out REST API & basic daemon logic

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
+import flask
 import numpy as np
 import os
 import signal
 import sys
 import time
 
+from gevent import event, pywsgi, signal
+
 from omni.isaac.kit import SimulationApp
+
+DEFAULT_POSE = [1, 0, 0, 0, 0, 0, 0]
 
 ROBOT_NAME = 'robot'
 ROBOT_PRIM_PATH = '/%s' % ROBOT_NAME
@@ -18,9 +23,114 @@ ROBOT_COMPONENTS = {
 }
 UPDATE_DELAY_SECS = 3.0
 
-map_path = os.environ.get('BENCHBOT_MAP_PATH')
-robot_path = os.environ.get('BENCHBOT_ROBOT_PATH')
-start_pose = os.environ.get('BENCHBOT_START_POSE')
+
+class SimulatorDaemon:
+
+    def __init__(self, port):
+        self.address = 'localhost:%d' % port
+
+        self.inst = None
+        self.sim = None
+
+        self.map_usd = None
+        self.robot_usd = None
+        self.start_pose = None
+
+    def open_usd(self):
+        # TODO figure out API calls for this...
+        pass
+
+        # TODO start simulation if appropriate
+
+    def place_robot(self):
+        p = DEFAULT_POSE if self.start_pose is None else self.start_pose
+
+        # Add robot to the environment at the requested pose
+        add_reference_to_stage(usd_path=self.robot_path,
+                               prim_path=ROBOT_PRIM_PATH)
+        r = Robot(prim_path=ROBOT_PRIM_PATH, name=ROBOT_NAME)
+        r.set_world_pose(position=p[4::] * 100, orientation=p[:4])
+
+        # TODO start simulation if appropriate
+
+    def run(self):
+        f = flask.Flask(__name__)
+
+        @f.route('/', methods=['GET'])
+        def __hello():
+            return flask.jsonify("Hello, I am the Omniverse Sim Daemon")
+
+        @f.route('/open_environment', methods=['POST'])
+        def __open_env():
+            # TODO process JSON
+            if self.inst is None:
+                print("No simulator running. Stored USD, but not opening.")
+                return
+            self.open_usd()
+
+        @f.route('/place_robot', methods=['POST'])
+        def __place_robot():
+            # TODO process JSON
+            if self.inst is None:
+                print(
+                    "No simulator running. Stored USD & pose, but not opening."
+                )
+                return
+            self.place_robot()
+
+        @f.route('/restart', methods=['POST'])
+        def __restart():
+            stop()
+            start()
+
+        @f.route('/start', methods=['POST'])
+        def __start():
+            start()
+
+        @f.route('/stop', methods=['POST'])
+        def __stop():
+            stop()
+
+        server = pywsgi.WSGIServer(self.address, f)
+
+    def start(self):
+        if not self.inst is None:
+            print("Instance already running. Please /stop first.")
+            return
+        env = {} if self.map_usd is None else {"open_usd": self.map_usd}
+        # Start the simulator
+        self.inst = SimulationApp({
+            "renderer": "RayTracedLighting",
+            "headless": False,
+            **env
+        })
+
+        # Import all required modules, and configure application
+        from omni.isaac.core import SimulationContext
+        from omni.isaac.core.robots import Robot
+        from omni.isaac.core.utils.extensions import enable_extension
+        from omni.isaac.core.utils.stage import add_reference_to_stage
+        from omni.kit.commands import execute
+        from omni.kit.viewport import get_default_viewport_window
+        from pxr import Sdf
+        enable_extension("omni.isaac.ros_bridge")
+
+        # Insert the robot if it is set
+        if self.robot_usd is not None:
+            self.place_robot()
+
+        # TODO start simulation if appropriate
+
+    def stop(self):
+        if self.inst is None:
+            print("No instance is running to stop.")
+            return
+        self.inst.close()
+        self.inst = None
+
+
+if __name__ == '__main__':
+    sd = SimulatorDaemon(port=os.environ.get('PORT'))
 
 
 def finish(sim_context, kit):
